028173a4a185a1ac4b759952a0476091af9d993b
Mikias Gedlu <addiserp@gmail.com>
2024-04-12 16:59:37 +0300

    0x00. Personal data 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -15,9 +15,10 @@ def hash_password(password: str) -> bytes:
     hashed = bcrypt.hashpw(bpass, bcrypt.gensalt())
     return hashed
 
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """
-        function that expects 2 arguments and returns a boolean.    
+        function that expects 2 arguments and returns a boolean.
     """
     if bcrypt.checkpw(b'password', hashed_password):
         return True
