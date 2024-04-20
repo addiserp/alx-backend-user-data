699825753f0cc4aaf7d1d9d8914c430e9d4786e2
Mikias Gedlu <addiserp@gmail.com>
2024-04-20 16:51:59 +0300

    0x01. Basic authentication 3

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,30 +1,33 @@
 #!/usr/bin/env python3
-""" Class for authentication
+"""
+A class module of an authentication
 """
 from flask import request
 
 
 class Auth:
     """
-        A class simple API authentication
+    A class for managing the API authentication
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-            A method that handles require auth
+        if endpoint requires auth this method will validat
         """
         if path is None or excluded_paths is None or excluded_paths == []:
             return True
 
     def authorization_header(self, request=None) -> str:
         """
-            A method that handles authorization header
+        the method authorization header for handling authorization
         """
         if request is None:
             return None
 
+        return request.headers.get("Authorization", None)
+
     def current_user(self, request=None) -> TypeVar('User'):
         """
-            It validates current user
+        It checks the current user
         """
         return None
