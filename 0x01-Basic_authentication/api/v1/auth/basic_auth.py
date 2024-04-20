4e488551ebdbf6e425ec220e10a895422f71cf72
Mikias Gedlu <addiserp@gmail.com>
2024-04-20 17:32:49 +0300

    0x01. Basic authentication 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,3 +11,19 @@ class BasicAuth(Auth):
     """
     A class for managing the Basic API authentication
     """
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+            A mothod for exctracting to base64
+        """
+        if authorization_header is None:
+            return None
+
+        if not isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        encoded = authorization_header.split(' ', 1)[1]
+
+        return encoded
